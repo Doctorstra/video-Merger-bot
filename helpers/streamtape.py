5ddb8470cb8b06206c390ed5997dbe2c20a6ed6f
Predator HackerzZ <77227205+PredatorHackerzZ@users.noreply.github.com>
2021-06-17 20:23:38 +0530

    streamtape.py

--- helpers/streamtape.py
+++ helpers/streamtape.py
@@ -1,4 +1,4 @@
-# (c) @AbirHasan2005
+# (c) @AbirHasan2005 | @PredatorHackerzZ
 
 import aiohttp
 from configs import Config
@@ -22,4 +22,4 @@ async def UploadToStreamtape(file: str, editable: Message, file_size: int):
             await editable.edit(text_edit, parse_mode="Markdown", disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Open Link", url=download_link)]]))
     except Exception as e:
         print(f"Error: {e}")
-        await editable.edit("Sorry, Something went wrong!\n\nCan't Upload to Streamtape. You can report at [Support Group](https://t.me/linux_repo)")
+        await editable.edit("Sorry, Something went wrong!\n\nCan't Upload to Streamtape. You can report at [Support Group](https://t.me/TeleRoid14)")
