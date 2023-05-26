2ffb040aed577a73bdd17824886047d5d6387655
Doctorstra <97329667+Doctorstra@users.noreply.github.com>
2023-05-26 23:31:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ class Bot(Client):
              api_hash=Config.API_HASH, 
              bot_token=Config.BOT_TOKEN, 
              workers=200, 
-             plugins={"root": "plugins"}, 
+             plugins={"root": "helper"}, 
              sleep_threshold=15, 
          )
 
