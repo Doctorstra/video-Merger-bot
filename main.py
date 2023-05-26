35a0a52389be796fc26fb1be8d7537c9d3a3bd29
Doctorstra <97329667+Doctorstra@users.noreply.github.com>
2023-05-26 19:11:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,13 +11,6 @@ import shutil
 import psutil
 import random
 import asyncio
-from datetime import datetime 
-from pytz import timezone 
-from pyrogram import Client, __version__ 
-from pyrogram.raw.all import layer 
-from config import Config 
-from aiohttp import web 
-from route import web_server
 from PIL import Image
 from configs import Config
 from pyromod import listen
